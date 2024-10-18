f567fff477039c99a78bffcd8e862254513c753c
Marko <marcoberich1991@gmail.com>
2024-10-18 11:57:46 +0200

    Add additional content to homepage

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-const toggleSwitch = document.getElementById('dark-mode-switch');
-const currentTheme = localStorage.getItem('theme');
-
-if (currentTheme) {
-document.body.classList.add(currentTheme);
-if (currentTheme === 'dark') {
-    toggleSwitch.checked = true; // Set the switch to checked if dark mode is enabled
-}
-}
-
-toggleSwitch.addEventListener('change', () => {
-if (toggleSwitch.checked) {
-    document.body.classList.add('dark');
-    document.body.classList.remove('light');
-    localStorage.setItem('theme', 'dark'); // Save the theme in local storage
-    document.querySelector('#dark-mode-switch + label').innerHTML = '<i class="fa-solid fa-moon"></i>';
-} else {
-    document.body.classList.remove('dark');
-    document.body.classList.add('light');
-    localStorage.setItem('theme', 'light'); // Save the theme in local storage
-    document.querySelector('#dark-mode-switch + label').innerHTML = '<i class="fa-solid fa-sun"></i>';
-}
-});
-
 document.addEventListener("DOMContentLoaded", function() {
     const section = document.getElementById('how-it-works');
     const steps = document.querySelectorAll('.step');
